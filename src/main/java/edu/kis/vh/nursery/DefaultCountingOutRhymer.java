10f31ec6bee07cc99b157e2d5a93766da24e7926
Kamil Dymecki <228729@edu.p.lodz.pl>
2022-01-10 11:14:35 +0100

    1.4 stale 2 - dodanie modyfikatoru final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,7 +6,7 @@ public class DefaultCountingOutRhymer {
     public static final int TOTAL_VALUE = -1;
     public static final int ERROR_VALUE = -1;
     public static final int FULL_VALUE = 11;
-    private int[] numbers = new int[NUMBER_SIZE];
+    private final int[] numbers = new int[NUMBER_SIZE];
 
     public int total = TOTAL_VALUE;
 
