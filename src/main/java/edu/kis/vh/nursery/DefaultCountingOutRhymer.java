64dea111ae18b8260b1f9948f452427f3e60615a
fedrige <fedrige@gmail.com>
2022-01-28 14:28:16 +0100

    1.5.6 - dodanie dokumentacji do klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Implementacja klasy DefaultCountingOutRhymer
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int NUMBER_SIZE = 12;
@@ -10,29 +13,53 @@ public class DefaultCountingOutRhymer {
 
     private int total = TOTAL_VALUE;
 
+    /**
+     * Zwracanie całkowitej wartości stosu
+     * @return całkowita wartość stosu
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * metoda dodająca wartość do stosu
+     * @param in wartość do dodania do stosu
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * Sprawdzanie czy stos jest pusty
+     * @return true jeśli jest pusty w przeciwnym razie false
+     */
     public boolean callCheck() {
         return total == TOTAL_VALUE;
     }
 
+    /**
+     * Sprawdzanie czy stos jest pełen
+     * @return true jeśli jest pełen w przeciwnym razie false
+     */
     public boolean isFull() {
         return total == FULL_VALUE;
     }
 
+    /**
+     * Zwraca najwyższą wartość stosu
+     * @return ostatni element stosu
+     */
     protected int peekaboo() {
         if (callCheck())
             return ERROR_VALUE;
         return numbers[total];
     }
 
+    /**
+     * Usuwa najwyższą wartość stosu
+     * @return ostatni element stosu
+     */
     public int countOut() {
         if (callCheck())
             return ERROR_VALUE;
